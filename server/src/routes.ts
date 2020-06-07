c4fe09d79c97f90255a67c98c9ea14483dd53d20
Pedro Lima <pedro.freitas.lima.97@gmail.com>
2020-06-07 00:53:48 -0300

    Add last alteration

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,7 @@ import ItemsController from './controllers/ItemsController';
 import PointsController from './controllers/PointsController';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 const routes = express.Router();
 const itemsController = new ItemsController();
@@ -11,7 +12,23 @@ const uploads = multer(multerConfig);
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', uploads.single('image'), pointsController.create);
+routes.post('/points', 
+uploads.single('image'),
+celebrate({
+    body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        state: Joi.string().required().max(2),
+        items: Joi.string().required()
+    })
+},{
+    abortEarly: false,
+}),
+pointsController.create);
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
 
